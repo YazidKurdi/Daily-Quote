8dc25a7f63da811c32f21d5eac8f7c8a6fbe044e
Yazid <yazeedkurdi@hotmail.com>
2022-12-15 21:55:58 +0300

    Quote of the day app

--- run.py
+++ run.py
@@ -5,5 +5,5 @@ from main.util.quote_api import get_quote
 if __name__ == "__main__":
     # scheduler.add_job(id = "scheduled task",func= get_quote,trigger = 'interval',seconds = 5)
     # scheduler.start()
-    app.run(debug=True)
+    app.run(host = "0.0.0.0", port = 5000)
 
