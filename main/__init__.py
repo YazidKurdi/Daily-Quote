9957fbe23d8c2d2f8dc6faa26f306b981baf4f6f
Yazid <yazeedkurdi@hotmail.com>
2022-12-16 12:27:08 +0300

    Removed un-nessacry code

--- main/__init__.py
+++ main/__init__.py
@@ -1,9 +1,7 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_apscheduler import APScheduler
 
 app = Flask(__name__)
-db = SQLAlchemy(app)
 scheduler = APScheduler()
 
 from main.app_page.routes import app_page
